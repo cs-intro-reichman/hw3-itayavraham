1da3ba1dcf7962d96279947d8d9c87a67e6ee782
unknown <itay.avraham01@post.runi.ac.il>
2024-11-18 20:16:34 +0200

    is the autograder supposed to only have 2 points or??

--- Anagram.java
+++ Anagram.java
@@ -29,22 +29,47 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		Boolean appearance = false;
+		str1 = preProcess(str1);
+		str2 = preProcess(str2);
+		for (int i = 0; i < str1.length(); i++) {
+			for (int j = 0; j < str2.length(); j++) {
+				if(str1.charAt(i) == str2.charAt(j)) {
+					appearance = true;				
+				}
+			}
+			if (appearance == false)
+			return false;
+			appearance = false;
+		}
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	private static String preProcess(String str) {
-		// Replace the following statement with your code
+		str = str.toLowerCase();
+		String newStr = "";
+		for (int i = 0; i < str.length(); i++)
+		{
+			if (str.charAt(i) >= 'a')
+			newStr += str.charAt(i);
+		}
+		str = newStr;
 		return str;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return str;
+		String newStr = "", remains = str;
+		int i;
+		while (remains.length() > 0) {
+			i = (int)(Math.random() * remains.length());
+			newStr += remains.charAt(i);
+			remains = remains.substring(0, i) + remains.substring(i + 1);
+		}
+		return newStr;
 	}
 }
