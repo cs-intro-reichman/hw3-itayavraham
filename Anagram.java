4e6de0faee86243d1335e708a0a9cb20bf7ff55a
unknown <itay.avraham01@post.runi.ac.il>
2024-11-19 19:05:05 +0200

    confusing instructions given regarding space handling

--- Anagram.java
+++ Anagram.java
@@ -55,7 +55,7 @@ public class Anagram {
 		// Creating a new string composed only of the smaller case letters.
 		for (int i = 0; i < str.length(); i++)
 		{
-			if (str.charAt(i) >= 'a')
+			if (str.charAt(i) >= 'a' || str.charAt(i) == ' ')
 			newStr += str.charAt(i);
 		}
 		str = newStr;
